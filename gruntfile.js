17226c53bf4bd401056a8fca0ce3b328cbd1096d
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2017-07-26 13:02:53 +0200

    first commit

---
+++ gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+  	sass: {
+        options: {
+            sourceMap: true
+        },
+        dist: {
+            files: {
+                'style.css': 'style.sass'
+            }
+        }
+    },
+
+    imagemin: {
+    dynamic: {
+        files: [{
+            expand: true,
+            cwd: 'images/',
+            src: ['**/*.{png,jpg,jpeg,gif}'],
+            dest: 'images/min/'
+        	}]
+    	}
+	},
+
+	watch: {
+    scripts: {
+        files: ['**/*.sass'],
+        tasks: ['sass'],
+        options: {
+            spawn: false,
+        	},
+    	}
+	}
+
+  });
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['sass', 'imagemin']);
+};
